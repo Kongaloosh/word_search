7f811bdef7afb70aa0d5845c30d7fbbde9c38b6f
Alex Kearney <konga.kearney@gmail.com>
2015-09-10 14:13:20 -0600

    flask app for presenting things as a site

--- domain_getter.py
+++ domain_getter.py
@@ -4,22 +4,26 @@ import whois
 
 __author__ = 'kongaloosh'
 
-results = open('results','r')
-domains = []
+def random_domain():
+    pass
 
-try:
-    while True:
-        domains.append(pickle.load(results))
 
-except EOFError:
-    print(len(domains))
-    while True:
-        pick = domains[random.randint(0, (len(domains))-1)]
-        for (word,tld) in pick:
-            try:
-                domain = word[:len(word)-len(tld)] + '.' + tld
-                if whois.whois(domain)["expiration_date"] == None:
-                    print(domain)
-                    exit(1)
-            except UnboundLocalError:
-                pass
+def domainify():
+    """Returns a random domain where the last characters form a TLD"""
+    results = open('results','r')
+    domains = []
+    try:
+        while True:
+            domains.append(pickle.load(results))
+
+    except EOFError:
+        print(len(domains))
+        while True:
+            pick = domains[random.randint(0, (len(domains))-1)]
+            for (word,tld) in pick:
+                try:
+                    domain = word[:len(word)-len(tld)] + '.' + tld
+                    if whois.whois(domain)["expiration_date"] == None:
+                        return(domain)
+                except UnboundLocalError:
+                    pass
