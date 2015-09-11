bb20a306be3d4d9ac62c84ca0498d92e32d03553
Alex Kearney <konga.kearney@gmail.com>
2015-09-10 19:29:40 -0600

    randomized the returned extension

--- domain_getter.py
+++ domain_getter.py
@@ -25,18 +25,17 @@ def domainify():
             print(pick[0])
             definition =  find(pick[0][0])
             if definition:
+                results = []
                 for (word,tld) in pick:
                     try:
                         domain = word[:len(word)-len(tld)] + '.' + tld
                         if whois.whois(domain)["expiration_date"]:
-                            return {'domain':domain, 'definition':definition}
+                            results.append({'domain':domain, 'definition':definition})
                     except (UnboundLocalError, KeyError):
                         pass
-                    except whois.parser.PywhoisError:           # this isn't 100% accurate
-                            return {'domain':domain, 'definition':definition}
-                    # except:
-                    #     pass
-
+                    # except whois.parser.PywhoisError:           # this isn't 100% accurate
+                    #     results.append({'domain':domain, 'definition':definition})
+                return results[random.randint(0, (len(results))-1)]
 def find(word):
     print(word)
     try:
